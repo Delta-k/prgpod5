7538415040169fcd01f3437fc7789dc23e644aa4
Deltawav-dev <A01730545@itesm.mx>
2020-10-29 20:17:15 +0000

    Tarea 2

--- hola.py
+++ hola.py
@@ -1,16 +1,40 @@
 # %%
-1+1
-
-
-# %%
 import pandas as pd
 import numpy as np
+import seaborn as sns
 # %%
-df = pd.read_csv("insurance.csv")
+df = pd.read_csv("ulabox_orders_with_categories_partials_2017.csv")
 df.head()
 
-# %%
-print("Esta es la media de la edad:", df["age"].mean())
+#%%
+
+sns.displot(x="weekday",hue="hour",multiple="stack",data=df)
+
+#%%
+g = sns.histplot(data=df, x="hour", multiple="stack", hue = "weekday")
+for q in df.hour.quantile([.25, .5, .75]):
+    g.axvline(q, linestyle=":")
+    g.text(q, 5, q)
 
 
 # %%
+sns.heatmap(df.corr())
+
+# %%
+df.plot.scatter(x="weekday",y="discount%")
+
+# %%
+df.plot.scatter(x="weekday",y="hour")
+
+# %%
+df.plot.scatter(x="weekday",y="Food%")
+
+
+# %%
+df.plot.scatter(x="weekday",y="Fresh%")
+
+# %%
+sns.catplot(x = "weekday", y =  "discount%", data = df, kind = "box");
+
+# %%
+sns.catplot(x = "weekday", y =  "discount%", data = df, kind = "box");
--- hola2.py
+++ hola2.py
@@ -1,3 +1,46 @@
-lista = [1,4,7,2,10]
-# ------- 
-print(lista)
+# %%
+import pandas as pd
+import numpy as np
+#%%
+import seaborn as sns
+
+# %%
+df = pd.read_csv("insurance.csv")
+df.head()
+df.describe() 
+
+#%%
+#sns.displot(x = "region", data = df)
+#sns.displot(x="region",hue="sex",multiple="stack",data=df)
+#sns.displot(x="age",data=df)
+#g=sns.histplot(data=df,x="age",multiple="stack",hue="sex")
+for q in df.age.quantile([.25,.5,.75]):
+    g.axvline(q,linestyle=":") #línea para marcar el eje en la posición q
+    g.text(q,5,q)
+
+#%%
+df.corr()
+#sns.heatmap(df.corr(),annot=True)
+#sns.scatterplot(data=df,x="age",y="charges")
+sns.scatterplot(data=df,x="bmi",y="charges",hue="smoker")
+
+#%%
+fumadores = df[df.smoker == "yes"]
+nofumadores = df[df.smoker == "no"]
+#sns.scatterplot(data=fumadores,x="age",y="charges",hue="bmi")
+#sns.heatmap(fumadores.corr(),annot=True)
+
+pd.cut(fumadores.bmi ,[0,18.5,25,30,60], labels=['Underweight', 'Normal', 'Overweight','Obese'], right = True)
+#cerrado por la izq
+
+
+#%%
+
+pd.cut(df.age,[17,20,35,50,64],labels=['Adolecente','Joven adulto','Adulto','Adulto mayor'])
+
+# %%
+print("Esta es la media de la edad:", df["age"].mean())
+
+
+# %%
+df.shape
