dfa975987c45e98e77159e3f19a9485178c737c8
Deltawav-dev <A01730545@itesm.mx>
2020-10-30 05:59:45 +0000

    Tarea 4 terminada

--- hola.py
+++ hola.py
@@ -1,40 +1,20 @@
 # %%
 import pandas as pd
 import numpy as np
-import seaborn as sns
+
 # %%
 df = pd.read_csv("ulabox_orders_with_categories_partials_2017.csv")
 df.head()
 
 #%%
-
-sns.displot(x="weekday",hue="hour",multiple="stack",data=df)
+df.shape
 
 #%%
-g = sns.histplot(data=df, x="hour", multiple="stack", hue = "weekday")
-for q in df.hour.quantile([.25, .5, .75]):
-    g.axvline(q, linestyle=":")
-    g.text(q, 5, q)
-
+df.describe()
 
 # %%
-sns.heatmap(df.corr())
+df.mean()
+df.median()
+df.mode()
 
 # %%
-df.plot.scatter(x="weekday",y="discount%")
-
-# %%
-df.plot.scatter(x="weekday",y="hour")
-
-# %%
-df.plot.scatter(x="weekday",y="Food%")
-
-
-# %%
-df.plot.scatter(x="weekday",y="Fresh%")
-
-# %%
-sns.catplot(x = "weekday", y =  "discount%", data = df, kind = "box");
-
-# %%
-sns.catplot(x = "weekday", y =  "discount%", data = df, kind = "box");
